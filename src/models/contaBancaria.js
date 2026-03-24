c9345228326649a178ad82e3c2d2518d6cccd59b
Marcoz V. S. <marcozviniciussilva@gmail.com>
2026-03-24 10:15:58 -0300

    model da conta bancaria criado

---
+++ src/models/contaBancaria.js
@@ -0,0 +1,18 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from '../database/db.js'
+
+const Conta = sequelize.define('Account', {
+  accountNumber: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  balance: {
+    type: DataTypes.DECIMAL(15, 2),
+    defaultValue: 0.00
+  },
+  type: {
+    type: DataTypes.ENUM('corrente', 'poupanca'),
+    defaultValue: 'corrente'
+  }
+});
