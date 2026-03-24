12b586241ba0a9411044d80c7806793617ec67f2
Marcoz V. S. <marcozviniciussilva@gmail.com>
2026-03-24 09:59:21 -0300

    conectando o banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log('Conexão com MySQL estabelecida com sucesso!');
+    } catch (error) {
+        console.error('Erro ao conectar ao banco:', error);
+    }
+}
+
+export default (sequelize, connect);
