600240db717a12d65919d17d07d0ce72b609c297
Marcoz V. S. <marcozviniciussilva@gmail.com>
2026-03-24 10:36:01 -0300

    criação do server.js

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}, http://localhost:${PORT}`));
